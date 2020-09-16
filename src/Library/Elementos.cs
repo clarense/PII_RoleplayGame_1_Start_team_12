68b0516945ab98be5c5113731ac08729b8886112
Valentina Cabrera <valentina.cabreram@correo.ucu.edu.uy>
2020-09-16 20:04:49 -0300

    Modificadas librerias para filtrar elementos por personajes

--- src/Library/Elementos.cs
+++ src/Library/Elementos.cs
@@ -1,12 +1,64 @@
 using System;
+using System.Collections.Generic;
 
 namespace elementos
 {
     public class Elementos
     {
-        public string TipoDeObjeto { get; set; }
-        public int Daño { get; set; }
-        public int Armadura { get; set; }
+        private string tipodeobjeto;
+        public string TipoDeObjeto
+        {
+            get
+            {
+                return this.TipoDeObjeto;
+            }
+            set
+            {
+                List<string> Tipos = new List<string>() {"Túnica", "Bastón", "Arco", "Armadura ligera", "Hacha", "Armadura"};
+                if (Tipos.Contains(tipodeobjeto))
+                {
+                    value = tipodeobjeto;
+                }
+            }
+        }
+        private int daño;
+        public int Daño
+        {
+            get
+            {
+                return this.Daño;
+            }
+            set
+            {
+                if (daño<0)
+                {
+                    value = 0;
+                }
+                else
+                {
+                    value = daño;
+                }
+            }
+        }
+        private int armadura;
+        public int Armadura
+        {
+            get
+            {
+                return this.Armadura;
+            }
+            set
+            {
+                if (armadura<0)
+                {
+                    value = 0;
+                }
+                else
+                {
+                    value = armadura;
+                }
+            }
+        }
 
     }
 }
--- src/Library/Personajes.cs
+++ src/Library/Personajes.cs
@@ -1,15 +1,148 @@
 using System;
+using System.Collections.Generic;
+using elementos;
 
 namespace personajes
 {
-   public class Personajes
+    public class Personajes
     {
         public string Nombre { get; set; }
-        public string Raza { get; set; }
-        public string Items { get; set; }
-        public int Vida { get; set; }
-        public int Ataque { get; set; }
-        public int Defensa { get; set; }
-
+        private string raza;
+        public string Raza
+        {
+            get
+            {
+                string texto = "";
+                switch(Raza)
+                {
+                    case "1":
+                        texto = "Mago";
+                        break;
+                    case "2":
+                        texto = "Elfo";
+                        break;
+                    case "3":
+                        texto = "Enano";
+                        break;
+                }
+                return texto;
+            }
+            set
+            {
+                switch(raza)
+                {
+                    case "Mago":
+                    case "mago":
+                        value = "1";
+                        break;
+                    case "Elfo":
+                    case "elfo":
+                        value = "2";
+                        break;
+                    case "Enano":
+                    case "enano":
+                        value = "3";
+                        break;
+                }
+            }
+        }
+        private Elementos item;
+        public List<Elementos> Items
+        {
+            get
+            {
+                return this.Items;
+            }
+            set
+            {
+                List<string> ItemsEquipables = null;
+                switch(Raza)
+                {
+                    case "1":
+                        ItemsEquipables = new List<string>() {"Túnica", "Bastón"};
+                        break;
+                    case "2":
+                        ItemsEquipables = new List<string>() {"Arco", "Armadura ligera"};
+                        break;
+                    case "3":
+                        ItemsEquipables = new List<string> () {"Hacha", "Armadura"};
+                        break;
+                }
+                if(ItemsEquipables.Contains(item.TipoDeObjeto))
+                {
+                    foreach(Elementos objeto in Items)
+                    {
+                        if(objeto.TipoDeObjeto == item.TipoDeObjeto)
+                        {
+                            Items.Remove(objeto);
+                            Items.Add(item);
+                            break;
+                        }
+                        else
+                        {
+                            Items.Add(item);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        private int vida;
+        public int Vida
+        {
+            get
+            {
+                return this.Vida;
+            }
+            set
+            {
+                if (vida<1)
+                {
+                    value = 1;
+                }
+                else
+                {
+                    value = vida;
+                }
+            }
+        }
+        private int ataque;
+        public int Ataque
+        {
+            get
+            {
+                return this.Ataque;
+            }
+            set
+            {
+                if (ataque<1)
+                {
+                    value = 1;
+                }
+                else
+                {
+                    value = ataque;
+                }
+            }
+        }
+        private int defensa;
+        public int Defensa
+        {
+            get
+            {
+                return this.Defensa;
+            }
+            set
+            {
+                if (defensa<0)
+                {
+                    value = 0;
+                }
+                else
+                {
+                    value = defensa;
+                }
+            }
+        }
     }
 }
